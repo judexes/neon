ab7df37ffe5a071d0fb70c15d349db4e48cca751
jude okoroafor <judeokoroafor@gmail.com>
2020-07-24 17:24:00 +0100

    Initial remote commit

---
+++ includes/dbconn.php
@@ -0,0 +1,12 @@
+
+<?php
+// UNCOMMENT ONLY AFTER SETTING UP DATABASE
+
+// if(!empty($_POST["send"])) {
+// 	$email = $_POST["userEmail"];
+
+// 	$conn = mysqli_connect("localhost", "root", "", "scan-tc_db") or die("Connection Error: " . mysqli_error($conn));
+// 	mysqli_query($conn, "INSERT INTO subscribers (email) VALUES ('" . $email. "')");
+// 	$insert_id = mysqli_insert_id($conn);
+// }
+?>
---
+++ index.php
@@ -0,0 +1,308 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        
+        <!-- Meta Tags -->
+        <title>Neon</title>
+        <meta name="title" content="Your Cyber Photographer">
+        <meta name="description" content="Your typical Cyber Photographer">
+        <meta name="author" content="Jude Okoroafor">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="robots" content="index, follow">
+        <meta name="language" content="English">
+        <meta name="keywords" content="Template, HTML, Cyber, Photographer">
+        <!-- Hero CSS -->
+        <!-- Bootstrap core CSS -->
+        <link href="style/vendors/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="style/responsive.css" rel="stylesheet">
+        <link href="style/main.css" rel="stylesheet">
+    </head>
+    <body class="text-color-1 bg-color-2">
+        <?php include "includes/dbconn.php"; // Using database connection file here ?>
+        <div id="top offcanvas-close"></div>
+        <!-- MENU SECTION -->
+        <section class="">
+        </section>
+        <!-- HERO SECTION -->
+        <section class="hero-section">
+            <!-- ------------------------ MAIN NAV ----------------------- -->
+            <div class="navbar-main">
+                
+                <nav class="navbar navbar-expand-lg container">
+                    <a class="navbar-brand mr-auto mr-lg-0 reload-page" href="#" id="Reloadpage" style="min-width: 80px;">
+                        <img src="img/logo.png" alt="Logo Image" id="logo">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse  navbar-collapse justify-content-end" id="navbarsExample08">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#our-services">About me</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#our-stylist">Portfolio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#profile">Blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div class="splash">
+                <div class="container">
+                    <h1 class="display-4 font-weight-bolder text-color-3">Cyber</h1>
+                    <h1 class="display-4 font-weight-bolder">Photography.</h1>
+                    <a class="btn btn-primary btn-lg text-uppercase" href="#" role="button">Book Session</a>
+                </div>
+                <div id="about"></div>
+            </div>
+        </section>
+        <!-- ABOUT SECTION -->
+        <section class="about-us-section">
+            
+            <div class="container">
+                
+                <div class="row text-center ">
+                    <div class="col-sm-6">
+                        <div class="about-us about-us-description ml-2 mr-2 text-left">
+                            <h6 class="subtitle font-weight-bold text-color-3">HELLO FREINDS</h6>
+                            <h2 class="title font-weight-bold">My name is Tom Hardy</h2>
+                            <p class="title-description">I can help you create positive and permanent changes in your life. High Performance Portfolio Template. Let’s Create Something new and awesome Togeather. I can help you create positive and permanent changes in your life..</p>
+                            <br>
+                            <p class="title-description">I can help you create positive and permanent changes in your life. High Performance Portfolio Template. Let’s Create Something new and awesome Togeather. I can help you create positive and permanent changes in your life..</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="about-us mb-3 mr-sm-0 ml-sm-0 profile">
+                            
+                            <img class="img-fluid" src="img/about.png">
+                        </div>
+                    </div>
+                </div>
+                <div id="services"></div>
+            </div>
+        </section>
+        <!-- TOOLS SECTION -->
+        <section class="tools-section">
+            <div class="tools-shade">
+                
+                <div class="container p-3">
+                    <div class="row text-center text-light">
+                        <div class="col-2 offset-1">
+                            <div class="counter">
+                                <img src="img/tool1.png" class="img-fluid mb-3">
+                                <p class="count-text ">Professional Skills</p>
+                            </div>
+                        </div>
+                        <div class="col-2">
+                            <div class="counter">
+                                <img src="img/tool2.png" class="img-fluid mb-3">
+                                <p class="count-text ">Unique Locations</p>
+                            </div>
+                        </div>
+                        <div class="col-2">
+                            <div class="counter">
+                                <img src="img/tool3.png" class="img-fluid mb-3">
+                                <p class="count-text ">Best Equiptment</p>
+                            </div>
+                        </div>
+                        <div class="col-2">
+                            <div class="counter">
+                                <img src="img/tool4.png" class="img-fluid mb-3">
+                                <p class="count-text ">Light Skills</p>
+                            </div>
+                        </div>
+                        <div class="col-2">
+                            <div class="counter">
+                                <img src="img/tool5.png" class="img-fluid mb-3">
+                                <p class="count-text ">458000+ Shots</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- SHOOTS SECTION -->
+        <section class="shoots-section ml-2 mr-2">
+            <div class="container text-center">
+                
+                <div class="row services">
+                    <div class="col-12 pb-md-4 text-left">
+                        <h6 class="subtitle font-weight-bold text-color-3">Services</h6>
+                        <h2 class="title font-weight-bold">Photography</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row text-center justify-content-center">
+                    <div class="photo-row">
+                        <div class="column">
+                            <img src="img/shoot1.png" class="img-fluid pb-1">
+                            <img src="img/shoot2.png" class="img-fluid pb-1">
+                        </div>
+                        
+                        <div class="column column-middle">
+                            <img src="img/shoot3.png" class="img-fluid">
+                        </div>
+                        
+                        <div class="column">
+                            <img src="img/shoot4.png" class="img-fluid pb-1">
+                            <img src="img/shoot5.png" class="img-fluid pb-1">
+                        </div>
+                    </div>
+                </div>
+                <div class="row text-center justify-content-center">
+                    <div class="photo-row">
+                        <div class="column column-middle column-50">
+                            <img src="img/shoot6.png" class="img-fluid">
+                        </div>
+                        <div class="column column-middle column-50">
+                            <img src="img/shoot7.png" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <div class="row text-center justify-content-center">
+                    <div class="photo-row">
+                        <div class="column column-25">
+                            <img src="img/shoot8.png" class="img-fluid">
+                        </div>
+                        <div class="column column-25">
+                            <img src="img/shoot9.png" class="img-fluid">
+                        </div>
+                        <div class="column column-25">
+                            <img src="img/shoot10.png" class="img-fluid">
+                        </div>
+                        <div class="column column-25">
+                            <img src="img/shoot11.png" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="portfolio"></div>
+        </section>
+        <!-- BLOG SECTION -->
+        <section class="blog-section">
+            <div class="container text-center">
+                <div class="row services">
+                    <div class="col-12 pb-md-4 text-left">
+                        <h6 class="subtitle font-weight-bold text-color-3">BLOG</h6>
+                        <h2 class="title font-weight-bold">My Journey</h5>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-sm-5">
+                        <div class="about-us mb-3 mr-sm-0 ml-sm-0">
+                            <img class="img-fluid" src="img/blog1.png">
+                        </div>
+                    </div>
+                    <div class="col-sm-7">
+                        <div class="about-us about-us-description ml-2 mr-2 text-left">
+                            <h6 class="subtitle font-weight-bold text-color-3">12 JUlY 2020</h6>
+                            <h6 class="title font-weight-bold">NEON LIGHTS: How I Started Photographing<br>in Colour.</h6>
+                            <p class="title-description">Lorem ipsum dolor sit amet, vix odio integre ei. Nulla minimum patrioque vix ad, id cum habeo delicata, ne minim comprehensam eos. Eu vim tempor aliquip. Docendi delectus insolens pri ei, no vel ludus putant persius.</p>
+                            <br>
+                            <p class="title-description">Facilis denique eam ut, ad accusamus cotidieque eos. Quem vivendum signiferumque in usu, ea eam quas aeque iriure. Solum voluptaria referrentur</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="services"></div>
+            </div>
+        </section>
+        <!-- conatct SECTION -->
+        <section class="contact-form-section">
+            <div class="container">
+                <div class="justify-content-around row">
+                    <div class="col-10 contact-form ">
+                        <div class="m-5">
+                            <h2 class="h1-responsive font-weight-bold text-left">Contact us</h2>
+                            <div class="row">
+                                <!--Grid column-->
+                                <div class="col-md-9 mb-md-0 mb-5">
+                                    <form id="contact-form" name="contact-form" action="#" method="POST">
+                                        <!--Grid row-->
+                                        <div class="row">
+                                            <!--Grid column-->
+                                            <div class="col-md-6">
+                                                <div class="md-form mb-0">
+                                                    <label for="name" class="">Name*</label>
+                                                    <input type="text" id="name" name="name" class="form-control">
+                                                </div>
+                                            </div>
+                                            <!--Grid column-->
+                                            <!--Grid column-->
+                                            <div class="col-md-6">
+                                                <div class="md-form mb-0">
+                                                    <label for="email" class="">Email*</label>
+                                                    <input type="text" id="email" name="email" class="form-control">
+                                                </div>
+                                            </div>
+                                            <!--Grid column-->
+                                        </div>
+                                        <!--Grid row-->
+                                        <!--Grid row-->
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="md-form mb-0">
+                                                    <label for="subject" class="">Subject*</label>
+                                                    <input type="text" id="subject" name="subject" class="form-control">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--Grid row-->
+                                        <!--Grid row-->
+                                        <div class="row">
+                                            <!--Grid column-->
+                                            <div class="col-md-12">
+                                                <div class="md-form">
+                                                    <label for="message">Your Message*</label>
+                                                    <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--Grid row-->
+                                    </form>
+                                    <div class="text-center text-md-left">
+                                        <a class="btn btn-primary">SEND</a>
+                                    </div>
+                                </div>
+                                <!--Grid column-->
+                                <!--Grid column-->
+                                <div class="col-md-3 text-center">
+                                    <ul class="list-unstyled mb-0">
+                                        <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                                            <p>San Francisco, CA 94126, USA</p>
+                                        </li>
+                                        <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                                            <p>+ 01 234 567 89</p>
+                                        </li>
+                                        <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                                            <p>contact@mdbootstrap.com</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <!--Grid column-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="services"></div>
+            </div>
+        </section>
+        <!-- TEST SECTION -->
+        <section class="test-section">
+        </section>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="javascript/vendors/bootstrap.bundle.js"></script>
+        <script src="javascript/script.js"></script>
+    </body>
+</html>
